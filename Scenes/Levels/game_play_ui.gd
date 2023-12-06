1cdd16322e63b5075b9f2b0a8175e8dc15b14dc8
Steven Coghill <76601239+BriarSMC@users.noreply.github.com>
2023-12-06 15:30:01 -0500

    Fixed health bar display

--- Scenes/Levels/game_play_ui.gd
+++ Scenes/Levels/game_play_ui.gd
@@ -79,8 +79,9 @@ func updateUI() -> void:
 	$Panel/ItemCounts/Coins.text = uiLabels[uiLabelType.COINS] + str(Globals.coinCount)
 	$Panel/ItemCounts/Gems.text = uiLabels[uiLabelType.GEMS] + str(Globals.gemCount)
 	$Panel/ItemCounts/ChallengersLeft.text = uiLabels[uiLabelType.CHALLENGERSLEFT] + str(Globals.challengersLeft)
-	$Panel/HealthBar.max_value = Globals.maxHealth
-	$Panel/HealthBar.value = Globals.health
+	$Panel/HealthGroup/HealthBar.max_value = Globals.maxHealth
+	$Panel/HealthGroup/HealthBar.value = Globals.health
+	$Panel/HealthGroup/HBLabels/maxHealth.text = str(Globals.maxHealth)
 
 func setColor(v: int, e: Label) -> void:
 	if v > 0:
